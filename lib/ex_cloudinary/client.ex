4b3a8233e5eba9b1a1769a0c370644687c78c78c
Jessica Schneider <jessica.schneider1207@gmail.com>
2016-04-05 21:03:57 -0400

    decode to structs

--- lib/ex_cloudinary/client.ex
+++ lib/ex_cloudinary/client.ex
@@ -1,16 +1,17 @@
 defmodule ExCloudinary.Client do
-  @moduledoc false
+  @moduledoc false  
   use HTTPoison.Base
+  @after_compile __MODULE__
   @base_url ~S(https://api.cloudinary.com/v1_1)
   @signed_params ~w(callback eager format from_public_id public_id tags timestamp to_public_id text transformation type)a
-  @name Application.get_env(:ex_cloudinary, :name)
+  @cloud_name Application.get_env(:ex_cloudinary, :cloud_name)
   @api_key Application.get_env(:ex_cloudinary, :api_key)
-  @api_secret Application.get_env(:ex_cloudinary, :api_secret)
+  @api_secret Application.get_env(:ex_cloudinary, :api_secret, <<0>>)
   
   ## HTTPoison.Base extensions
 
   @doc false
-  def process_url(url), do: "#{@base_url}/#{@name}/#{url}"
+  def process_url(url), do: "#{@base_url}/#{@cloud_name}/#{url}"
 
   @doc false
   def process_request_body(body) do
@@ -21,7 +22,7 @@ defmodule ExCloudinary.Client do
   end
 
   @doc false
-  def process_response_body(body), do: Poison.decode!(body)
+  #def process_response_body(body), do: Poison.decode!(body)
 
   ## Private helpers
   
@@ -57,5 +58,23 @@ defmodule ExCloudinary.Client do
     body = Enum.map(body, fn {:file, path} -> {:file, path}
                               {key, value} -> {to_string(key), value} end)
     {:multipart, body}
+  end
+  
+  def __after_compile__(env, _bytecode) do
+    if is_nil(@cloud_name), do: raise error_msg("cloud name")
+    if is_nil(@api_key), do: raise error_msg("api_key")
+    if @api_secret == <<0>>, do: raise error_msg("api_secret")
+  end
+  
+  defp error_msg(missing) do
+  ~s"""
+  Missing config for `#{missing}`.
+  
+  Ensure your `config.exs` is set according to the following:
+    config :ex_cloudinary,
+      cloud_name: "",
+      api_key: "",
+      api_secret: ""
+  """
   end
 end
